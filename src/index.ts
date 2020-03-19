204967eecc64630c67403fd5478ca3332149a594
Ilya <44939563+zabozlaev@users.noreply.github.com>
2020-03-19 21:09:22 +0300

    fix dotenv module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { logger } from './api/utils/logger';
 import { app } from './app';
-import 'dotenv';
+import 'dotenv/config';
 
 app.listen(+process.env.PORT, '0.0.0.0', (err, addr) => {
   if (err) logger.error(err);
