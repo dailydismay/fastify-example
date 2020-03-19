9f5be233bade5cfedb93685a81455ede95f962ec
Ilya Zabcode <ilya@belkins.io>
2020-03-19 20:39:29 +0300

    readme

--- dist/api/handlers/hello.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloHandler = async ({ headers }, reply) => reply.send({ headers });
-//# sourceMappingURL=hello.js.map
